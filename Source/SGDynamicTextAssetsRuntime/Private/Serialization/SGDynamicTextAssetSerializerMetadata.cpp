ebb1037dc9cb19ed86329974ccd0c6d2e9e8c83b
Nicholas Helish <nhelish23@hotmail.com>
2026-06-06 12:47:55 -0700

    fix: Miss removal of files that should have been renamed in runtime

--- Source/SGDynamicTextAssetsRuntime/Private/Serialization/SGDynamicTextAssetSerializerMetadata.cpp
+++
@@ -1,8 +0,0 @@
-﻿// Copyright Start Games, Inc. All Rights Reserved.
-
-#include "Serialization/SGDynamicTextAssetSerializerMetadata.h"
-
-bool FSGDynamicTextAssetSerializerMetadata::IsValidId() const
-{
-	return SerializerFormat.IsValid();
-}
